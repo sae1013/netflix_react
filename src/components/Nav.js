de1cffc8b41f1f077e7c0e90d6068b4ac465e9e1
minwoo <sae1013@gmail.com>
2021-09-30 02:55:04 +0900

    FEAT:Complete NavBarComponent

---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from 'react'
+import classes from './Nav.module.css';
+import logo from '../images/logo.png';
+import avatar from '../images/avatar.png';
+
+function Nav() {
+  const [NavShow,handleNavShow] = useState(true);
+
+  const transitionNavBar = () => {
+    if(window.scrollY > 90) {  // transparent
+      handleNavShow(false);
+    }else {
+      handleNavShow(true);
+    }
+  }
+  useEffect(()=>{
+    const scrollEvent = window.addEventListener("scroll",transitionNavBar);
+    
+    return ()=>{
+      window.removeEventListener("scroll",scrollEvent);
+    }
+  },[])
+  return ( 
+    <div className={`${classes.nav} ${!NavShow && classes.bg_black}`}>
+      <div className={classes.nav_items}>
+        <div className={classes.nav_logo}>
+            <img src={logo}></img>
+        </div>
+        <div className={classes.nav_avatar}>
+            <img src={avatar}></img>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/page/HomeScreen.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Nav from '../components/Nav';
+
+function Homescreen() {
+  return (
+    <div>
+      <Nav></Nav>      
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      <p style={{fontSize:"20px"}}>TEST</p>
+      {/* Banner */}
+      
+      {/* Row  */}
+    </div>
+  )
+}
+
+export default Homescreen
